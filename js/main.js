0dd37ad403270df2f7e985bda36e95f59b71ebc5
Valerie Mettler <valeriemettler@gmail.com>
2015-10-06 00:08:46 -0700

    add on play, pause, and end event listener, display notes algorithm, add user note input

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+var timedNotes = [];
 var tag = document.createElement('script');
 
 tag.src = "https://www.youtube.com/iframe_api";
@@ -12,15 +13,105 @@ function onYouTubeIframeAPIReady() {
         width: '640',
         videoId: 'TpKR3KXpg_E',
         events: {
-            'onReady': onPlayerReady,
-            // 'onStateChange': onPlayerStateChange
+            'onStateChange': onPlayerStateChange,
+            'onError': onPlayerError,
+            'onReady': getPlayerTime
         }
     });
-}
+};
 
-function onPlayerReady(event) {
-    event.target.playVideo();
-}
+function getPlayerTime() {
+    var t = cleanTime();
+    //console.log(notes(t));
+    $('#x').html(notes(t));
+    setTimeout(getPlayerTime, 1000);
+};
+
+function notes(t) {
+    if (t < 56) {
+        return "Am";
+    } else if (t < 57) {
+        return "G";
+    } else if (t < 59) {
+        return "C";
+    } else if (t < 61) {
+        return "D";
+    } else if (t < 63) {
+        return "F";
+    } else if (t < 66) {
+        return "G/Am";
+    } else {
+        return "Am";
+    }
+};
+
+
+function onPlayerStateChange(event) {
+    // switch (event.data) {
+    //     case YT.PlayerState.PLAYING:
+    //         if (cleanTime() == 0) {
+    //             console.log('started ' + cleanTime());
+    //         } else {
+    //             console.log('playing ' + cleanTime())
+    //         };
+    //         break;
+    //     case YT.PlayerState.PAUSED:
+    //         if (player.getDuration() - player.getCurrentTime() != 0) {
+    //             console.log('paused' + ' @ ' + cleanTime());
+    //         break;
+    //     case YT.PlayerState.ENDED:
+    //         console.log('ended ');
+    //         break;
+    // };
+};
+//utility
+function cleanTime() {
+    return Math.round(player.getCurrentTime())
+};
+
+var setHandlers = function() {
+    $("#addNote").on('click', function(event) {
+        event.stopPropagation();
+        var note = $('#note').val();
+        // $('#x').html(note);
+        //console.log(note, cleanTime());
+        timedNotes.push([cleanTime(), note]);
+        console.log(timedNotes);
+    });
+};
+setHandlers();
+
+function onPlayerError(event) {
+    // console.log("error");
+    // switch(event.data) {
+    //     case 2:
+    //         console.log('' + video.id)
+    //         ga('send', 'event', 'video', 'invalid id',video);
+    //         break;
+    //     case 100:
+    //         ga('send', 'event', 'video', 'not found',video);
+    //         break;
+    //     case 101 || 150:
+    //         ga('send', 'event', 'video', 'not allowed',video);
+    //         break;
+    //     };
+};
+
+// function onPlayerReady(event) {
+//     event.target.playVideo();
+//     console.log("player ready");
+// }
+
+
+// function onPlayerStateChange(event) {
+//     console.log(player.getPlayerState());
+//     if (event.data == YT.PlayerState.PLAYING) {
+//         console.log("Video started playing");
+//     }
+// }
+
+
+
 // var done = false;
 
 // function onPlayerStateChange(event) {
